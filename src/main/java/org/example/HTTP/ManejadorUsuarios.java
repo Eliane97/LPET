ea81e0f37818aed1f2224c268a89a412c15e6e7c
godoy <godoyeli85@gmail.com>
2023-05-02 18:03:51 -0300

    servidor http creado

---
+++ src/main/java/org/example/HTTP/ManejadorUsuarios.java
@@ -0,0 +1,14 @@
+package org.example.HTTP;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class ManejadorUsuarios implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
+}
